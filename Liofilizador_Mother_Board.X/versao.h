4e03b4a513f34cb0b421ec34069e2ff4460f9464
Neraildes <neraildes@gmail.com>
2019-11-24 20:18:19 -0300

    Versao final numero v1.0.9

--- Liofilizador_Mother_Board.X/versao.h
+++ Liofilizador_Mother_Board.X/versao.h
@@ -1,6 +1,6 @@
 // arquivo criado pelo script versionar.sh
-// Neraildes - Fri, 22 Nov 2019 19:41:18 -0300
+// Neraildes - Sun, 24 Nov 2019 16:25:40 -0300
 #ifndef VERSAO_H
 #define	VERSAO_H
-#define FVERSION "v1.0.8_Beta"
+#define FVERSION "v1.0.9"
 #endif
