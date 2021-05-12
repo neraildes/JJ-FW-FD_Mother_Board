781c9e9a4229cf7c23cb385adbce02048ab81a45
neraildes <neraildes@gmail.com>
2021-05-12 09:51:49 -0300

    commitar para trocar de branch

--- Liofilizador_Mother_Board.X/versao.h
+++ Liofilizador_Mother_Board.X/versao.h
@@ -1,6 +1,6 @@
 // arquivo criado pelo script versionar.sh
-// neraildes - Fri, 16 Apr 2021 16:12:19 -0300
+// neraildes - Fri, 16 Apr 2021 16:15:11 -0300
 #ifndef VERSAO_H
 #define	VERSAO_H
-#define FVERSION "v1.0.31"
+#define FVERSION "v1.0.32_beta"
 #endif
