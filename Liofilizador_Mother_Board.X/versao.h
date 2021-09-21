4a8584d3d988ce129f2b8022aa59121a54c2d23b
neraildes <neraildes@gmail.com>
2021-09-21 09:45:14 -0300

    Commit inicial para display vertical

--- Liofilizador_Mother_Board.X/versao.h
+++ Liofilizador_Mother_Board.X/versao.h
@@ -1,6 +1,6 @@
 // arquivo criado pelo script versionar.sh
-// neraildes - Wed, 12 May 2021 09:32:52 -0300
+// neraildes - Tue, 21 Sep 2021 09:42:39 -0300
 #ifndef VERSAO_H
 #define	VERSAO_H
-#define FVERSION "v1.0.33-beta"
+#define FVERSION "v1.0.34-beta"
 #endif
