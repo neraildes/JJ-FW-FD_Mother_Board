71c15d837facd4c4842436540cbbf3e0ffe2a17b
neraildes <neraildes@gmail.com>
2021-10-09 16:02:40 -0300

    Primeiro commit da versão.

--- Liofilizador_Mother_Board.X/versao.h
+++ Liofilizador_Mother_Board.X/versao.h
@@ -1,6 +1,6 @@
 // arquivo criado pelo script versionar.sh
-// neraildes - Wed, 6 Oct 2021 19:42:33 -0300
+// neraildes - Sat, 9 Oct 2021 15:57:33 -0300
 #ifndef VERSAO_H
 #define	VERSAO_H
-#define FVERSION "v1.0.37"
+#define FVERSION "v1.0.38"
 #endif
