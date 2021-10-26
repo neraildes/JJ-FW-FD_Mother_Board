ffd01623a08480b69ddc06c9fc68903ea76b5bd5
Neraildes da Silva <neraildes@gmail.com>
2021-10-25 23:01:15 -0300

    Primeiro commit da versao atual.

--- Liofilizador_Mother_Board.X/build/default/production/Liofilizador Placa Mae.i
+++ Liofilizador_Mother_Board.X/build/default/production/Liofilizador Placa Mae.i
@@ -9015,7 +9015,7 @@ void ShowHardwareInfo(){
      totalboard=0;
      strcpy(texto,"");
      strcat(texto,"* : Mother Board ");
-     strcat(texto,"v1.0.47");
+     strcat(texto,"v1.0.48");
      print(texto);
 
      for(destino=1;destino<15;destino++)
--- Liofilizador_Mother_Board.X/versao.h
+++ Liofilizador_Mother_Board.X/versao.h
@@ -1,6 +1,6 @@
 // arquivo criado pelo script versionar.sh
-// Neraildes da Silva - Mon, 25 Oct 2021 22:47:31 -0300
+// Neraildes da Silva - Mon, 25 Oct 2021 22:57:09 -0300
 #ifndef VERSAO_H
 #define	VERSAO_H
-#define FVERSION "v1.0.47"
+#define FVERSION "v1.0.48"
 #endif
