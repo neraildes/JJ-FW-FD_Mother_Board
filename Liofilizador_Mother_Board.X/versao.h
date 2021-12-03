da22dabffdf2f8e95f7756409eff83635bd5c21c
Neraildes da Silva <neraildes@gmail.com>
2021-12-03 04:53:27 -0300

    VERSÃO PETRUZ COM ALTERAÇÃO NA LEITURA DO VACCUO

--- Liofilizador_Mother_Board.X/versao.h
+++ Liofilizador_Mother_Board.X/versao.h
@@ -1,6 +1,6 @@
 // arquivo criado pelo script versionar.sh
-// Neraildes da Silva - Thu, 2 Dec 2021 19:13:12 -0300
+// Neraildes da Silva - Fri, 3 Dec 2021 04:46:40 -0300
 #ifndef VERSAO_H
 #define	VERSAO_H
-#define FVERSION "v1.0.60"
+#define FVERSION "v1.0.61_Petruz"
 #endif
